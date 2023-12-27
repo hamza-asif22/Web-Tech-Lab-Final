14dd2e617569e19c8b8fad7ea64d7236199265f4
hamza-asif22 <hamzaasifbaig@gmail.com>
2023-12-27 17:22:39 +0500

    Web Tech Lab Final

--- labfinal/src/App.js
+++ labfinal/src/App.js
@@ -1,52 +1,25 @@
 import React from 'react';
-import {
-  Route, Routes, NavLink,
-} from 'react-router-dom';
-import Dragons from './components/Dragons';
-import Missions from './components/Missions/Missions';
-import Rockets from './components/Rockets';
-import MyProfile from './components/MyProfile';
-import planet from './assets/images/planet.png';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+import DragonsList from './routes/DragonsList';
+import MissionsList from './routes/MissionsList';
+import MyProfile from './routes/MyProfile';
 
 function App() {
   return (
-    <>
-      <nav className="bg-light border-bottom border-success navbar navbar-expand-md navbar-light">
-        <div className="container">
-          <div>
-            <img src={planet} alt="planet" className="planet" />
-          </div>
-          <div>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav mr-auto gap-3">
-                <li className="nav-item">
-                  <NavLink to="/" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>Rockets</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/missions" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>Missions</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/dragons" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>Dragons</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/myprofile" className={(navData) => (navData.isActive ? 'active-style' : 'none')}>My Profile</NavLink>
-                </li>
-              </ul>
-            </div>
-          </div>
-          {/* Menu burger */}
-          <button className="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span className="navbar-toggler-icon" />
-          </button>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <h1>SpaceX Explorer</h1>
+          <Routes>
+            <Route path="/missions" element={<MissionsList />} />
+            <Route path="/dragons" element={<DragonsList />} />
+            <Route path="/myprofile" element={<MyProfile />} />
+          </Routes>
         </div>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/dragons" element={<Dragons />} />
-        <Route path="/myprofile" element={<MyProfile />} />
-      </Routes>
-    </>
+      </Router>
+    </Provider>
   );
 }
 

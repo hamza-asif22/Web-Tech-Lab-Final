51c95546b8f78407c988bfc53453a8f0a461576d
hamza-asif22 <hamzaasifbaig@gmail.com>
2023-12-27 17:36:24 +0500

    Lab Final

--- labfinal/src/routes/dragon.js
+++ labfinal/src/routes/dragon.js
@@ -4,7 +4,13 @@ const DragonsList = () => {
   return (
     <div>
       <h2>Dragons List</h2>
-      {/* Your DragonsList component content */}
+      <ul>
+        {dragons.map((dragon) => (
+          <li key={dragon.id}>
+            {dragon.name} - {dragon.reserved ? 'Reserved' : 'Available'}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- labfinal/src/routes/mission.js
+++ labfinal/src/routes/mission.js
@@ -4,7 +4,23 @@ const MissionsList = () => {
   return (
     <div>
       <h2>Missions List</h2>
-      {/* Your MissionsList component content */}
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>Join/Leave</th>
+        </tr>
+      </thead>
+      <tbody>
+        {missions.map((mission) => (
+          <tr key={mission.id}>
+            <Mission mission={mission} />
+          </tr>
+        ))}
+      </tbody>
+    </Table>
     </div>
   );
 };
